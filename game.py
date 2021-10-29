f171fe72c0b39980777720071ffd388322551ad6
Robert Barsan <robertbarsanalexandru@gmail.com>
2021-10-29 15:39:04 +0200

    Alpha_02
    Tout marche sauf le menu, ce serz dans l'apha_02-, ensuite je me charge de corriger les bugs d'anim avec l'image je vais faire du découpage du 32 sur 32
    
    Co-Authored-By: Geniegeekeur <92394236+Geniegeekeur@users.noreply.github.com>

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 import pytmx
 import pyscroll
+import time
 
 from perso import Player
 
@@ -8,31 +9,78 @@ class Game:
 
     def __init__(self):
         self.screen = pygame.display.set_mode((800, 800))
-        pygame.display.set_caption("Titre du jeu")
-
+        pygame.display.set_caption("Island of Kingdoms")
         tmx_data = pytmx.util_pygame.load_pygame('carte.tmx')
         map_data = pyscroll.data.TiledMapData(tmx_data)
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
         map_layer.zoom = 2
-
+        
         player_pos = tmx_data.get_object_by_name("player_spawn")
         self.player = Player(player_pos.x, player_pos.y)
 
         self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=4)
         
         self.group.add(self.player)
+    
+    def screen_update(self):
+        self.group.update()
+        self.group.center(self.player.rect.center)
+        self.group.draw(self.screen)
+        pygame.display.flip()
 
     def handle_input(self):
         pressed = pygame.key.get_pressed()
-
         if pressed[pygame.K_UP] or pressed[pygame.K_z]:
             self.player.move_up()
+            self.player.animations('./b/b1.png')
+            self.screen_update()
+            time.sleep(0.07)
+            self.player.move_up()
+            self.player.animations('./b/b2.png')
+            self.screen_update()
+            time.sleep(0.07)
+            self.player.move_up()
+            self.player.animations('./b/b3.png')
+            self.screen_update()
+           
         elif pressed[pygame.K_DOWN] or pressed[pygame.K_s]:
             self.player.move_down()
+            self.player.animations('./f/f1.png')
+            self.screen_update()
+            time.sleep(0.07)
+            self.player.move_down()
+            self.player.animations('./f/f2.png')
+            self.screen_update()
+            time.sleep(0.07)
+            self.player.move_down()
+            self.player.animations('./f/f3.png')
+            self.screen_update()
+            
         elif pressed[pygame.K_RIGHT] or pressed[pygame.K_d]:
             self.player.move_right()
+            self.player.animations('./r/r1.png')
+            self.screen_update()
+            time.sleep(0.07)
+            self.player.move_right()
+            self.player.animations('./r/r2.png')
+            self.screen_update()
+            time.sleep(0.07)
+            self.player.move_right()
+            self.player.animations('./r/r3.png')
+            self.screen_update()
+            
         elif pressed[pygame.K_LEFT] or pressed[pygame.K_q]:
             self.player.move_left()
+            self.player.animations('./l/l1.png')
+            self.screen_update()
+            time.sleep(0.07)
+            self.player.move_left()
+            self.player.animations('./l/l2.png')
+            self.screen_update()
+            time.sleep(0.07)
+            self.player.move_left()
+            self.player.animations('./l/l3.png')
+            self.screen_update()
 
     def run(self):
 
--- perso.py
+++ perso.py
@@ -4,12 +4,35 @@ class Player(pygame.sprite.Sprite):
 
     def __init__(self, x, y):
         super().__init__()
-        self.sprite_sheet = pygame.image.load('perso.png')
-        self.image = self.get_pos(0, 0)
+        self.sprite_sheet = pygame.image.load('./characters_model/char.png')
+        self.image = self.get_img(31, 0)
         self.image.set_colorkey([255, 255, 255])
         self.rect = self.image.get_rect()
         self.position = [x, y]
-        self.speed = 2
+        self.images_pos = {
+            './f/f1.png' : self.get_img(0, 0),
+            './f/f2.png' : self.get_img(32, 0),
+            './f/f3.png' : self.get_img(64, 0),
+
+            './l/l1.png' : self.get_img(0, 32),
+            './l/l2.png' : self.get_img(32, 32),
+            './l/l3.png' : self.get_img(64, 32),
+
+            './r/r1.png' : self.get_img(0, 64),
+            './r/r2.png' : self.get_img(32, 64),
+            './r/r3.png' : self.get_img(64, 64),
+
+            './b/b1.png' : self.get_img(0, 96),
+            './b/b2.png' : self.get_img(32, 96),
+            './b/b3.png' : self.get_img(64, 96),
+        }
+        self.speed = 4
+    def animations(self, anim_name):
+        self.image = self.images_pos[anim_name]
+        self.image.set_colorkey([255, 255, 255])
+        self.rect = self.image.get_rect()
+        self.update()
+    
 
     def move_right(self): self.position[0] += self.speed
     def move_left(self): self.position[0] -= self.speed
@@ -17,13 +40,15 @@ class Player(pygame.sprite.Sprite):
     def move_down(self): self.position[1] += self.speed
 
 
+
     def update(self):
         self.rect.topleft = self.position
 
 
-    def get_pos(self, x, y):
-        image = pygame.Surface([32, 32])
+    def get_img(self, x, y):
+        image = pygame.Surface([31, 31])
         image.blit(self.sprite_sheet, (0, 0), (x, y, 32, 32))
         return image
+
 
     
