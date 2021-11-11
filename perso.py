923863d66112c4b925d57ac66f1ea53aa23e6447
Geniegeekeur <92394236+Geniegeekeur@users.noreply.github.com>
2021-11-11 19:43:27 +0100

    Alpha 4 b (ou 2)
    Ajout des ressources (bois et pierre) + temps attente entre chaque 20 ressource du même type récoltée.

---
+++ perso.py
@@ -0,0 +1,58 @@
+from time import sleep
+import pygame
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self, x, y):
+        super().__init__()
+        self.sprite_sheet = pygame.image.load('perso.png')
+        self.image = self.get_img(34, 0)
+        self.image.set_colorkey([255, 255, 255])
+        self.rect = self.image.get_rect()
+        self.position = [x, y]
+        self.images_pos = {
+            'bas1' : self.get_img(0, 0),
+            'bas2' : self.get_img(32, 0), 
+            'bas3' : self.get_img(64, 0),
+
+            'gauche1' : self.get_img(0, 32),
+            'gauche2' : self.get_img(32, 32), 
+            'gauche3' : self.get_img(64, 32),
+
+            'droite1' : self.get_img(0, 64),
+            'droite2' : self.get_img(32, 64), 
+            'droite3' : self.get_img(64, 64),
+
+            'haut1' : self.get_img(0, 96),
+            'haut2' : self.get_img(32, 96), 
+            'haut3' : self.get_img(64, 96),
+        }
+        self.feet = pygame.Rect(0,0, self.rect.width * 0.5, 12)
+        self.old_position = self.position.copy()
+        self.speed = 8
+
+    def animations(self, anim_name):
+        self.image = self.images_pos[anim_name]
+        self.image.set_colorkey([255, 255, 255])
+        self.rect = self.image.get_rect()
+        self.update()
+
+    def move_right(self): self.position[0] += self.speed
+    def move_left(self): self.position[0] -= self.speed
+    def move_up(self): self.position[1] -= self.speed
+    def move_down(self): self.position[1] += self.speed
+
+    def save_location(self): self.old_position = self.position.copy()
+
+    def update(self):
+        self.rect.topleft = self.position
+        self.feet.midbottom = self.rect.midbottom
+
+    def move_back(self):
+        self.position = self.old_position
+        self.update()
+
+    def get_img(self, x, y):
+        image = pygame.Surface([32, 32])
+        image.blit(self.sprite_sheet, (0, 0), (x, y, 32, 32))
+        return image
