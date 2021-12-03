739859be6793e3e81917fabd8e95598c27db8296
Geniegeekeur <92394236+Geniegeekeur@users.noreply.github.com>
2021-12-03 21:35:41 +0100

    Delete maison.tsx

--- maison.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="maison" tilewidth="16" tileheight="16" tilecount="350" columns="25">
- <image source="maison.png" trans="ffffff" width="400" height="224"/>
-</tileset>
